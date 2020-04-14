55428deb93f919b5ba6ebbeda049f145ba0c0cb8
John Taggart <taggartj1985@gmail.com>
2020-04-14 16:21:25 +0100

    added name price and collection and tests pass

---
+++ models/park.js
@@ -0,0 +1,7 @@
+const Park = function (name, price, collection){
+  this.name = name;
+  this.price = price;
+  this.collection = [];
+}
+
+module.exports = Park;
---
+++ specs/park_spec.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+
+  let park
+
+  beforeEach(function () {
+    park = new Park('Isla', 50, [])
+  })
+
+  it('should have a name', function(){
+    const actual = park.name;
+    assert.strictEqual(actual, 'Isla');
+  });
+
+  it('should have a ticket price', function(){
+    const actual = park.price;
+    assert.strictEqual(actual, 50);
+  });
+
+  it('should have a collection of dinosaurs', function(){
+    const actual = park.collection;
+    assert.deepStrictEqual(actual, [])
+  });
+
+  it('should be able to add a dinosaur to its collection');
+
+  it('should be able to remove a dinosaur from its collection');
+
+  it('should be able to find the dinosaur that attracts the most visitors');
+
+  it('should be able to find all dinosaurs of a particular species');
+
+  it('should be able to calculate the total number of visitors per day');
+
+  it('should be able to calculate the total number of visitors per year');
+
+  it('should be able to calculate total revenue for one year');
+
+});
